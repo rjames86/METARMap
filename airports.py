cc215d086b12e94c4b124436be0ccbe2216da6d1
Ryan M <ryan@ryanmo.co>
2024-01-20 16:18:30 -0700

    testing

--- airports.py
+++ airports.py
@@ -91,6 +91,6 @@ def get_airport_codes():
     with open(os.path.join(os.getcwd(), 'airports')) as f:
         airports = f.readlines()
     from random import choices
-    return choices([x.strip() for x in airports if x.strip()], 50)
+    return choices([x.strip() for x in airports if x.strip()], k=50)
 
 AIRPORT_CODES = get_airport_codes()
