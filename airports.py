5aaa595737d295bc3d62185985ad80d82bd8c390
Ryan M <ryan@ryanmo.co>
2024-01-21 14:06:43 -0700

    testing

--- airports.py
+++ airports.py
@@ -99,13 +99,14 @@ class AirportLED:
             time.sleep(delay)
 
     async def fade(self):
-        logger.info("Fading %s" % self.airport_code)
+        logger.info("Fading %s. Index %s" % (self.airport_code, self.pixel_index))
         current_color = self.color
         # G, R, B = self.color
         # ALL_COLORS = [(G * 0.5, R * 0.5, B * 0.5) , self.color]
         ALL_COLORS = [BLACK , self.color]
         await sleep(0)
         while True:
+            logger.info("fading to %s" % (self.current_color))
             for color in ALL_COLORS:
                 await self.fade_pixel(0.3, self.pixel_index, current_color)
                 current_color = color
