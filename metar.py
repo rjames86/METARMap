7432a0deee003ab71c39abbf2925e584ed60b64f
Ryan M <ryan@ryanmo.co>
2024-01-21 14:16:24 -0700

    run a different way with async

--- metar.py
+++ metar.py
@@ -4,20 +4,28 @@ from constants import strip
 from metar_data import get_metar_data
 
 async def run():
+    loop = asyncio.get_event_loop()
     metar_infos = get_metar_data()
     print([(index, airport_code) for index, airport_code in enumerate(AIRPORT_CODES)])
     airport_leds = [AirportLED(strip, index, airport_code, metar_infos.get(airport_code)) for index, airport_code in enumerate(AIRPORT_CODES)]
     print(airport_leds)
-    tasks = [airport_led.run() for airport_led in airport_leds]
-    results = asyncio.gather(*tasks)
+    tasks = [loop.create_task(airport_led.run()) for airport_led in airport_leds]
     try:
-        await results
+        await asyncio.gather(*tasks)
     except Exception as e:
         print("Cancelling")
-        results.cancel()
+        [task.cancel() for task in tasks]
+        await asyncio.gather(*tasks)
         raise e
+
+print("Starting...")
+# asyncio.run(run())
+loop = asyncio.get_event_loop()
+task = loop.create_task(run())
+
 try:
-    print("Starting...")
-    asyncio.run(run())
+    loop.run_forever()
 except Exception as e:
+    task.cancel()
+    loop.run_until_complete(task)
     raise e
