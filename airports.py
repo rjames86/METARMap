26c86685641098082145b910a6113ce411ff8996
Ryan M <ryan@ryanmo.co>
2024-01-21 10:28:48 -0700

    update path for airports file

--- airports.py
+++ airports.py
@@ -1,6 +1,7 @@
 import os
 
 def get_airport_codes():
-    with open(os.path.join(os.getcwd(), 'airports')) as f:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, 'airports')) as f:
         airports = f.readlines()
     return [x.strip() for x in airports if x.strip()]
