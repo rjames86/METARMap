3d34126366a00d1f54143d96cb2464a2fc8eb610
Ryan M <ryan@ryanmo.co>
2024-01-21 10:28:12 -0700

    progress

--- airports.py
+++ airports.py
@@ -6,7 +6,7 @@ from constants import BLACK, FLIGHT_CATEGORY_TO_COLOR, WHITE
 import astral
 from astral.sun import sun as AstralSun
 import datetime
-
+from logger import logger
 
 
 class AirportLED:
@@ -20,62 +20,75 @@ class AirportLED:
             self.color = WHITE
             self.city = None
         else:
-            self.color = FLIGHT_CATEGORY_TO_COLOR.get(self.metar_info.flightCategory, WHITE)
-            self.city = astral.LocationInfo(timezone='UTC', latitude=self.metar_info.latitude, longitude=self.metar_info.longitude)
-
+            self.color = FLIGHT_CATEGORY_TO_COLOR.get(
+                self.metar_info.flightCategory, WHITE
+            )
+            self.city = astral.LocationInfo(
+                timezone="UTC",
+                latitude=self.metar_info.latitude,
+                longitude=self.metar_info.longitude,
+            )
 
     def __repr__(self):
-        return f'AirportLED<{self.airport_code}>'
-    
+        return f"AirportLED<{self.airport_code}>"
+
     """
     Figure out how bright the light should be based on time of day
     """
+
     def determine_brightness(self, color):
         if self.city is None:
             return color
-        G, R, B = color
-        dimming_level = 1
 
-        print(self.airport_code, self.metar_info.observation_time, self.metar_info.observation_time.tzinfo)
+        G, R, B = color
+
+        MIN_BRIGHTNESS = 0.1
+        dimming_level = 1
 
         try:
             sun = AstralSun(self.city.observer, tzinfo=self.city.tzinfo)
-            print(sun)
-            print(self.metar_info.observation_time < sun['dawn'], self.metar_info.observation_time > sun['dusk'])
-            # It's probably dark out
-            if self.metar_info.observation_time < sun['dawn'] or self.metar_info.observation_time > sun['dusk']:
-                print("before dawn or after dusk")
-                dimming_level = 0.1
-            elif sun['dawn'] < self.metar_info.observation_time < sun['noon']:
-                print("between dawn and noon")
-                total_seconds = sun['noon'] - sun['dawn']
-                seconds_until_noon = sun['noon'] - self.metar_info.observation_time
-                dimming_level = seconds_until_noon / total_seconds
-            elif sun['noon'] < self.metar_info.observation_time < sun['dusk']:
-                print("between noon and dusk")
-                total_seconds = sun['dusk'] - sun['noon']
-                seconds_until_dusk = sun['dusk'] - self.metar_info.observation_time
-                dimming_level = seconds_until_dusk / total_seconds
-                print('set dim level to', dimming_level)
+            dawn = sun["dawn"]
+            noon = sun["noon"]
+            dusk = sun["dusk"]
+
+            if (
+                self.metar_info.observation_time < dawn
+                or self.metar_info.observation_time > dusk
+            ):
+                dimming_level = MIN_BRIGHTNESS
+            elif dawn < self.metar_info.observation_time < noon:
+                total_seconds = noon - dawn
+                seconds_until_noon = noon - self.metar_info.observation_time
+                dimming_level = max(
+                    1 - (seconds_until_noon / total_seconds), MIN_BRIGHTNESS
+                )
+            elif noon < self.metar_info.observation_time < dusk:
+                total_seconds = dusk - noon
+                seconds_until_dusk = dusk - self.metar_info.observation_time
+                dimming_level = max(seconds_until_dusk / total_seconds, MIN_BRIGHTNESS)
             return (G * dimming_level, R * dimming_level, B * dimming_level)
         except Exception as e:
-            print("Failed set brightness", self.airport_code, self.metar_info.observation_time)
-            return WHITE
-
-
+            logger.error(
+                "Failed set brightness",
+                self.airport_code,
+                self.metar_info.observation_time,
+            )
+            return color
 
     async def run(self):
         while True:
-            print("running for ", self.airport_code)
+            logger.info("running for ", self.airport_code)
             if self.metar_info is None:
-                print("no metar info found for %s. Returning..." % self.airport_code)
+                logger.info(
+                    "no metar info found for %s. Returning..." % self.airport_code
+                )
                 await sleep(0)
                 return
             if self.metar_info.flightCategory is None:
                 await sleep(0)
                 return
-            
-            print(
+
+            logger.info(
                 "Setting light "
                 + str(self.pixel_index)
                 + " for "
@@ -88,9 +101,12 @@ class AirportLED:
             self.strip[self.pixel_index] = self.determine_brightness(self.color)
             await sleep(0)
 
+
 def get_airport_codes():
-    with open(os.path.join(os.getcwd(), 'airports')) as f:
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(dir_path, "airports")) as f:
         airports = f.readlines()
     return [x.strip() for x in airports if x.strip()]
+
 
 AIRPORT_CODES = get_airport_codes()
