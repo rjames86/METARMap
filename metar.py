93da17c98e0f66cfaecb5cae43b3e2e57fef692a
Ryan M <ryan@ryanmo.co>
2024-01-21 14:31:04 -0700

    revert asyncio changes

--- metar.py
+++ metar.py
@@ -4,28 +4,20 @@ from constants import strip
 from metar_data import get_metar_data
 
 async def run():
-    loop = asyncio.get_event_loop()
     metar_infos = get_metar_data()
     print([(index, airport_code) for index, airport_code in enumerate(AIRPORT_CODES)])
     airport_leds = [AirportLED(strip, index, airport_code, metar_infos.get(airport_code)) for index, airport_code in enumerate(AIRPORT_CODES)]
     print(airport_leds)
-    tasks = [loop.create_task(airport_led.run()) for airport_led in airport_leds]
+    tasks = [airport_led.run() for airport_led in airport_leds]
+    results = asyncio.gather(*tasks)
     try:
-        await asyncio.gather(*tasks)
+        await results
     except Exception as e:
         print("Cancelling")
-        [task.cancel() for task in tasks]
-        await asyncio.gather(*tasks)
+        results.cancel()
         raise e
-
-print("Starting...")
-# asyncio.run(run())
-loop = asyncio.get_event_loop()
-task = loop.create_task(run())
-
 try:
-    loop.run_forever()
+    print("Starting...")
+    asyncio.run(run())
 except Exception as e:
-    task.cancel()
-    loop.run_until_complete(task)
     raise e
