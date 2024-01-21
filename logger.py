f79beb2206315e5d4a06c41964e357b4185d8893
Ryan M <ryan@ryanmo.co>
2024-01-21 10:28:19 -0700

    logger

---
+++ logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def setup_logger():
+    # Create a logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a stream handler (outputs to stdout) and set the level to DEBUG
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and set it for the handler
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
+
+    # Add the stream handler to the logger
+    logger.addHandler(stream_handler)
+
+    return logger
+
+logger = setup_logger()
