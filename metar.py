25a1e31eae39532a1f7e084979f2cfe2b4ed5eb4
Ryan M <ryan@ryanmo.co>
2024-01-26 07:47:00 -0700

    testing

--- metar.py
+++ metar.py
@@ -1,16 +1,27 @@
 from airports import AirportLED, AIRPORT_CODES
 from constants import strip
 from metar_data import get_metar_data
+import time
+from logger import logger
 
-def run(metar_infos):
+def run():
+    now = time.time()
+
+    metar_infos = get_metar_data()
     airport_leds = [AirportLED(strip, index, airport_code, metar_infos.get(airport_code)) for index, airport_code in enumerate(AIRPORT_CODES)]
 
     while True:
         print("\n\nNEW LOOP\n\n")
+
+        if time.time() - now >= 60: # 1 minute
+            logger.info("Refreshing metar info")
+            metar_infos = get_metar_data()
+            airport_leds = [AirportLED(strip, index, airport_code, metar_infos.get(airport_code)) for index, airport_code in enumerate(AIRPORT_CODES)]
+            now = time.time()
+
         for airport_led in airport_leds:
             airport_led.set_pixel_color()
         strip.show()
 
 if __name__ == "__main__":
-    metar_infos = get_metar_data()
-    run(metar_infos)
+    run()
