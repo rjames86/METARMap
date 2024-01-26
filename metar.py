77f512df3ec8e5bdf49e12976a4abec107e3cb2b
Ryan M <ryan@ryanmo.co>
2024-01-26 06:34:53 -0700

    add sleep

--- metar.py
+++ metar.py
@@ -1,6 +1,7 @@
 from airports import AirportLED, AIRPORT_CODES
 from constants import strip
 from metar_data import get_metar_data
+from time import sleep
 
 def run():
     metar_infos = get_metar_data()
@@ -11,6 +12,7 @@ def run():
         for airport_led in airport_leds:
             airport_led.set_pixel_color()
         strip.show()
+        sleep(0.3)
 
 if __name__ == "__main__":
     run()
